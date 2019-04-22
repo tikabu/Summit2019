d5cc837cbd6bd7433164245604a8094a5dc29068
Adam Murray <adam@tikabu.com.au>
2019-04-22 22:14:24 +1000

    Initial code for demo

---
+++ src/Demo.ps1
@@ -0,0 +1,14 @@
+iwr -uri http://localhost:50436/swagger/v1/swagger.json -OutFile swagger.json -UseDefaultCredentials -AllowUnencryptedAuthentication
+
+autorest --powershell --input-file:./swagger.json --clear-output-folder --namespace:Times.Wire.Search
+.\generated\build-module.ps1 -run
+
+get-addomain
+
+get-device
+
+
+[system.appdomain]::CurrentDomain.GetAssemblies() | where location -like '*glueapi*'
+([system.appdomain]::CurrentDomain.GetAssemblies() | where location -like '*glueapi*').gettypes() | where name -eq 'AdDomainApiViewModel'
+(([system.appdomain]::CurrentDomain.GetAssemblies() | where location -like '*glueapi*').gettypes() | where name -eq 'AdDomainApiViewModel').getmembers() | ft name, membertype
+(([system.appdomain]::CurrentDomain.GetAssemblies() | where location -like '*glueapi*').gettypes() | where name -eq 'DeviceApiViewModel1').getmembers() | ft name, membertype
