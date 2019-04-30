cd7374ea332180cb7f2af628b5c1220313218b64
Adam Murray <adam@tikabu.com.au>
2019-04-29 21:40:09 -0700

    Add resetDemo

--- src/ResetDemo.ps1
+++ src/ResetDemo.ps1
@@ -1,2 +1,8 @@
 uninstall-module xkcd -Force
 #Remove xkcd from Azure Devops - https://dev.azure.com/apr2019/Workshop/_packaging?_a=feed&feed=Test
+
+
+rm C:\repos\Summit2019\src\xkcd\generated
+
+code c:\repos\summit2019
+code c:\repos\summit2019_muzz
