cc5636cd7f58cab63be1d7a9d4e1bbf7c463b2dc
Adam Murray <adam@tikabu.com.au>
2019-04-29 22:11:15 -0700

    Update demo

--- src/ResetDemo.ps1
+++ src/ResetDemo.ps1
@@ -2,7 +2,10 @@ uninstall-module xkcd -Force
 #Remove xkcd from Azure Devops - https://dev.azure.com/apr2019/Workshop/_packaging?_a=feed&feed=Test
 rm C:\Users\AdamMurray\Documents\WindowsPowerShell\modules\xkcd -recurse -force
 
-rm C:\repos\Summit2019\src\xkcd\generated
+rm C:\repos\Summit2019\src\xkcd\generated -Recurse -Force
 
 code c:\repos\summit2019
 code c:\repos\summit2019_muzz
+
+Clear-Host
+"Ready to go"
