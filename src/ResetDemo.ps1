9ac17680cf813a93c1444fe01de3d2a531ccdf98
Adam Murray <adam@tikabu.com.au>
2019-05-01 10:40:37 -0700

    Updates

--- src/ResetDemo.ps1
+++ src/ResetDemo.ps1
@@ -1,4 +1,5 @@
 uninstall-module xkcd -Force
+copy 'C:\repos\Summit2019\src\Visual Studio Code Host_history.txt' 'C:\Users\AdamMurray\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine' -force
 #Remove xkcd from Azure Devops - https://dev.azure.com/apr2019/Workshop/_packaging?_a=feed&feed=Test
 rm C:\Users\AdamMurray\Documents\WindowsPowerShell\modules\xkcd -recurse -force
 
